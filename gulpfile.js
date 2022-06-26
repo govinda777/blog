8030e0b154798be0472221cba67149890e189c98
Luã Govinda <govinda777@gmail.com>
2022-06-26 20:22:07 -0300

    instalacao pacotes gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,13 @@ const rename = require('gulp-rename');
 const clean = require('gulp-clean');
 
 exports.default = function() {
-  return src('[BACKLOG]*.md')
+  return src('*.md')
         
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file',
             context: { arr: ['test1', 'test2'] }
         }))
-        .pipe(rename('README.md'))
         .pipe(clean({force: true}))
         .pipe(dest('.'));
 }
-
-/*
-const gulp = require('gulp');
-
-gulp.task('default', function() {
-  gulp.src('*.md')
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('output/'));
-});*/
